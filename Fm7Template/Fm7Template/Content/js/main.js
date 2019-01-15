3021810b53e798c0b5ac72ee272d4757a75ea863
mahdiit <67848+mahdiit@users.noreply.github.com>
2019-01-15 16:40:24 +0330

    fix

---
+++ Fm7Template/Fm7Template/Content/js/main.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../../framework7/js/framework7.bundle.min.js" />
+
+var app = new Framework7({
+    root: '#app',
+    name: 'Awesome App',
+    id: 'mahdi.template.fm7',
+    panel: {
+        swipe: 'left'
+    },
+    routes: [
+        {
+            path: '/about/',
+            url: 'about.html'
+        }
+    ],
+    theme: 'md'
+});
+var mainView = app.views.create('.view-main');
+
+//app.on('init',
+//    function() {
+//        app.statusbar.hide();
+//    });
